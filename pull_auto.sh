f6b43c361e7c9c1d1a4474d76368723764775a63
ysyx_22050043-Geng Haoran <hrgeng@smail.nju.edu.cn>
2022-09-13 22:15:45 +0800

    auto update

---
+++ pull_auto.sh
@@ -0,0 +1,9 @@
+git pull
+cp -i .bashrc ~/.bashrc
+cp -i .condarc ~/.condarc
+cp -i .gitconfig ~/.gitconfig
+cp -i .inputrc ~/.inputrc
+cp -i .randomquotes ~/.randomquotes
+cp -i .tmux.conf ~/.tmux.conf
+cp -i .vimrc ~/.vimrc
+cp -i .Xmodmap ~/.Xmodmap
