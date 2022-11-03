58d56e559c703c53689cd12e4c74ecb59a9973c8
ysyx_22050043-Geng Haoran <hrgeng@smail.nju.edu.cn>
2022-11-03 21:30:52 +0800

    minecraft gpu support

--- ubuntu/minecraft/.minecraft/launch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# run minecraft with NVIDIA-gpu
-# ubuntu doesn't have prime-run. use script given in
-# https://blog.csdn.net/zjh919/article/details/127415588
-/usr/bin/prime-run /usr/bin/java "$@"
-
